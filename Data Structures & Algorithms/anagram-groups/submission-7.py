e2d556cc466908943d6c97e7d587ae1c9e524109
Jing <ktfwyCJ@gmail.com>
2026-06-19 14:02:15 +0200

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        counts = {}
+
+        for s in strs:
+            key = "".join(sorted(s))
+            if key in counts:
+                counts[key].append(s)
+            else:
+                counts[key] = [s]
+        
+        return list(counts.values())
